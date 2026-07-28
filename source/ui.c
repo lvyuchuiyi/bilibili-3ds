a43058a54c0952a5ab123bfe680c9ae9d635fd93
debug <debug@bilibili-3ds.local>
2026-07-28 11:59:24 +0800

    bisect/step1: font loading C2D_FontLoadSystem(1)

--- source/ui.c
+++ source/ui.c
@@ -2,21 +2,24 @@
 #include <citro2d.h>
 
 static C3D_RenderTarget *top = NULL;
-static C3D_RenderTarget *bot = NULL;
+static C2D_Font font = NULL;
 
 int ui_init(void) {
     gfxInitDefault();
     C3D_Init(0x100000);
     C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
     C2D_Prepare();
-    
-    // 第一步：只创建顶屏
+
+    font = C2D_FontLoadSystem(1);
+    if (!font) return -1;
+
     top = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
     if (!top) return -1;
     return 0;
 }
 
 void ui_exit(void) {
+    if (font) C2D_FontFree(font);
     C2D_Fini();
     C3D_Fini();
     gfxExit();
