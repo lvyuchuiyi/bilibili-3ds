355c068ab2b1f20c3b04e1683a562ff7067a389c
debug <debug@bilibili-3ds.local>
2026-07-29 17:06:16 +0800

    step4: ui.c with bitmap font

--- source/ui.h
+++ source/ui.h
@@ -1,3 +1,10 @@
-#ifndef _UI_H_ 
-#define _UI_H_ 
-#endif 
+#ifndef _UI_H_
+#define _UI_H_
+#include <3ds.h>
+void draw_char(int x,int y,char ch,u32 col);
+void draw_str(int x,int y,u32 col,const char*s);
+void draw_rect(int x,int y,int w,int h,u32 col);
+int ui_init(void);
+void ui_exit(void);
+void ui_render(void);
+#endif
