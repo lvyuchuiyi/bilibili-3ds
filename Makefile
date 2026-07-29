a1bc4866ae160f8a83e43b09406c0ab44ff6b22b
debug <debug@bilibili-3ds.local>
2026-07-29 23:32:27 +0800

    test: minimal socInit, no curl

--- Makefile
+++ Makefile
@@ -34,7 +34,7 @@ CXXFLAGS := $(CFLAGS) -fno-rtti -fno-exceptions -std=gnu++11
 ASFLAGS := -g $(ARCH)
 LDFLAGS  = -specs=3dsx.specs -g $(ARCH) -Wl,-Map,$(notdir $*.map) 
 
-LIBS := -lcitro2d -lcitro3d -lcurl -lmbedtls -lmbedx509 -lmbedcrypto -lctru -lm -lz
+LIBS := -lcitro2d -lcitro3d -lmbedtls -lmbedx509 -lmbedcrypto -lctru -lm -lz
 
 LIBDIRS := $(CTRULIB) $(PORTLIBS)
 
--- source/main.c
+++ source/main.c
@@ -1,28 +1,26 @@
 #include <3ds.h>
-#include <citro2d.h>
-#include <stdio.h>
-#include "network.h"
+#include <string.h>
+
+static u32 *m = NULL;
 
 int main(void) {
-    gfxInitDefault(); C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
-    C2D_Init(4096); C2D_Prepare(); gfxSet3D(false);
-    C3D_RenderTarget *t = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
-    if (!t) return 1;
+    gfxInitDefault();
+    m = (u32*)linearAlloc(0x100000);
+    int ret = -99;
+    if (m) {
+        ret = R_SUCCEEDED(socInit(m, 0x100000)) ? 0 : -2;
+    }
 
-    int ret = net_init();
-    u32 col;
-    if (ret == 0) col = C2D_Color32(0,255,0,255);
-    else if (ret == -1) col = C2D_Color32(255,0,0,255);
-    else if (ret == -2) col = C2D_Color32(255,255,0,255);
-    else col = C2D_Color32(0,0,255,255);
+    /* Display result - use raw framebuffer without citro2d */
+    u8 *fb = gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL);
+    u32 color = (ret == 0) ? 0x0000FF00 : 0x000000FF;
+    for (int i = 0; i < 400 * 240; i++) ((u32*)fb)[i] = color;
+    gfxSwapBuffers();
 
     while (aptMainLoop()) {
-        hidScanInput(); if (hidKeysDown() & KEY_START) break;
-        C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
-        C2D_TargetClear(t, C2D_Color32(0,0,0,0xFF));
-        C2D_SceneBegin(t);
-        C2D_DrawRectSolid(0, 0, 0.5f, 400, 240, col);
-        C3D_FrameEnd(0);
+        hidScanInput();
+        if (hidKeysDown() & KEY_START) break;
     }
-    C2D_Fini(); C3D_Fini(); gfxExit(); return 0;
+    gfxExit();
+    return 0;
 }
