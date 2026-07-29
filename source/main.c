45fc64db9fe0c16b4ab5a32bf99118d493d37b9c
debug <debug@bilibili-3ds.local>
2026-07-29 17:29:19 +0800

    step6d: player_init only

--- source/main.c
+++ source/main.c
@@ -1,9 +1,9 @@
 #include <3ds.h> 
 #include "ui.h" 
-#include "network.h" 
+#include "player.h" 
 int main(void){ 
 if(ui_init()!=0)return 1; 
-net_init(); 
+player_init(); 
 while(aptMainLoop()){ 
 hidScanInput();if(hidKeysDown()&KEY_START)break; 
 ui_render(NULL);} 
